6295495d849d5984597d3750d014c54abc1b5720
fxml21 <f20190351@pilani.bits-pilani.ac.in>
2022-04-13 19:53:39 +0530

    jar error

--- src/main/java/com/example/finman/model/Initializer.java
+++ src/main/java/com/example/finman/model/Initializer.java
@@ -4,9 +4,9 @@ import com.example.finman.utility.ScriptRunner;
 import org.sqlite.SQLiteConfig;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -15,20 +15,24 @@ import java.util.Objects;
 public class Initializer {
     private final String DB_URL = "jdbc:sqlite:finance.db";
 
-    public void initializeTables() throws SQLException, IOException {
+    public void initializeTables() throws SQLException, IOException, URISyntaxException {
         SQLiteConfig config = new SQLiteConfig();
         config.enforceForeignKeys(true);
         Connection connection;
         connection = DriverManager.getConnection(DB_URL, config.toProperties());
         try {
             ScriptRunner runner = new ScriptRunner(connection, false, false);
+//            System.out.println(getClass().getResource("").toURI());
             runner.runScript(new BufferedReader(
-                    new FileReader(Objects.requireNonNull(Initializer.class.getResource(
-                            "/sql/ddl/financeDDL.sql")).getFile())));
+                    new InputStreamReader(Objects.requireNonNull(
+                            Initializer.class.getClassLoader().getResourceAsStream(
+                                    "sql/ddl/financeDDL.sql")))));
 
             runner.runScript(new BufferedReader(
-                    new FileReader(Objects.requireNonNull(Initializer.class.getResource(
-                            "/sql/ddl/financeInitialData.sql")).getFile())));
+                    new InputStreamReader(Objects.requireNonNull(
+                            Initializer.class.getClassLoader().getResourceAsStream(
+                                    "sql/ddl/financeInitialData.sql")))));
+
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
